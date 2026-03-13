39fd5221f7c0f3de7c703388f481a9fe14fb9ae2
jiohslive <yashpisal8353@gmail.com>
2026-03-13 10:14:19 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -118,12 +118,11 @@ async def start(client, message):
             gtxt = "ɢᴏᴏᴅ ᴇᴠᴇɴɪɴɢ 🌘"
         else:
             gtxt = "ɢᴏᴏᴅ ɴɪɢʜᴛ 🌑"
-        m=await message.reply_text("👀")
+        msg = await message.reply_text("👀")
         await asyncio.sleep(1)
         await msg.edit_text("⚡")
         await asyncio.sleep(1)
-        await msg.edit_text("🦋")
-        await m.delete()        
+        await msg.edit_text("🦋")        
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, gtxt, temp.U_NAME, temp.B_NAME),
@@ -153,12 +152,11 @@ async def start(client, message):
             gtxt = "ɢᴏᴏᴅ ᴇᴠᴇɴɪɴɢ 🌘"
         else:
             gtxt = "ɢᴏᴏᴅ ɴɪɢʜᴛ 🌑"
-        m=await message.reply_text("👀")
+        msg = await message.reply_text("👀")
         await asyncio.sleep(1)
         await msg.edit_text("⚡")
         await asyncio.sleep(1)
-        await msg.edit_text("🦋")
-        await m.delete()        
+        await msg.edit_text("🦋")        
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, gtxt, temp.U_NAME, temp.B_NAME),

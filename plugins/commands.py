075721672bb9940adc1f3b9b8e85398d249464a4
jiohslive <yashpisal8353@gmail.com>
2026-03-13 10:26:06 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -124,7 +124,6 @@ async def start(client, message):
         await asyncio.sleep(1)
         await msg.edit_text("🦋") 
         await asyncio.sleep(1)
-        await m.delete()
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, gtxt, temp.U_NAME, temp.B_NAME),
@@ -160,7 +159,6 @@ async def start(client, message):
         await asyncio.sleep(1)
         await msg.edit_text("🦋")
         await asyncio.sleep(1)
-        await m.delete()
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, gtxt, temp.U_NAME, temp.B_NAME),
